13000678276a677aa262de675606c9c495ac0192
Alaricode <antonlarichev@gmail.com>
2026-01-30 10:30:36 +0300

    pydantic-settings

---
+++ app/core/settings.py
@@ -0,0 +1,30 @@
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppSettings(BaseModel):
+    app_name: str = "Board API"
+    debug: bool = False
+
+
+class DatabaseSettings(BaseModel):
+    url: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+    databese_url: str = Field(validation_alias="DATABASE_URL")
+
+    @property
+    def app(self) -> AppSettings:
+        return AppSettings()
+
+    @property
+    def db(self) -> DatabaseSettings:
+        return DatabaseSettings(url=self.databese_url)
