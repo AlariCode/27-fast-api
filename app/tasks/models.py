421b9af7f0a09b863831dd6a44fd4ab1f77bea40
Alaricode <antonlarichev@gmail.com>
2026-02-13 10:51:38 +0300

    Упражнение - Модель задачи

---
+++ app/tasks/models.py
@@ -0,0 +1,13 @@
+# id, title, descripton, is_completed
+from sqlalchemy import String, Text, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.db import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    is_completed: Mapped[bool] = mapped_column(Boolean, default=False)
